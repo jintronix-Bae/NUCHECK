6c988ce88b8a04d28634f9367a2b39f1ecb1223f
youngjin <252892903+jintronix-Bae@users.noreply.github.com>
2026-08-13 23:35:41 +0900

    feat: Initial Spring Boot 세팅

---
+++ src/main/java/nucheck/NucheckApplication.java
@@ -0,0 +1,13 @@
+package nucheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NucheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NucheckApplication.class, args);
+	}
+
+}
